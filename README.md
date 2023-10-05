8474b1d5d4e938741111eb2e9e705ad40f1415ac
Saint Wish <SaintWish@users.noreply.github.com>
2023-10-05 12:31:12 -0600

    add license and ccmap

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 A collection of a few Go packages for Key Value storage like caches.
 
 ## Packages
+All of the packages of generic support for safety.
+
 * `kv1` - A Key Value sharded cache with time expiration using the swiss map.
 * `kv1s` - A Key Value sharded cache using swiss map without any auto eviction.
 * `kv2` - A Key Value sharded cache with a max size and automated eviction using the swiss map.
+* `kvmap` - A Key Value sharded cache using vanilla Go map with no auto eviction.
+* `ccmap` - A concurrent safe map without sharding.
 * `stack` - A last in, first out stack implementation without concurrency support. Used in the ``kv2`` package.
---
+++ ccmap/ccmap.go
@@ -0,0 +1,91 @@
+package ccmap
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Cache[K comparable, V any] struct {
+	Data map[K]V //cached items
+	OnEvicted func(K, V) //function that's called when cached item is deleted automatically
+
+	sync.RWMutex //mutex
+}
+
+func New[K comparable, V any]() *Cache[K, V] {
+	return &Cache[K, V] {
+		Data: make(map[K]V, 0),
+	}
+}
+
+func (c *Cache[K, V]) SetOnEvicted(f func(K, V)) {
+	c.OnEvicted = f
+}
+
+func (c *Cache[K, V]) Get(key K) (data V) {
+	c.RLock()
+
+	data,_ = c.Data[key]
+
+	c.RUnlock()
+	return
+}
+
+func (c *Cache[K, V]) GetHas(key K) (data V, ok bool) {
+	c.RLock()
+
+	data, ok = c.Data[key]
+
+	c.RUnlock()
+	return
+}
+
+func (c *Cache[K, V]) Has(key K) (ok bool) {
+	c.RLock()
+
+	_, ok = c.Data[key];
+
+	c.RUnlock()
+	return
+}
+
+func (c *Cache[K, V]) Set(key K, val V) {
+	c.Lock()
+
+	c.Data[key] = val
+
+	c.Unlock()
+}
+
+func (c *Cache[K, V]) Add(key K, val V) error {
+	if c.Has(key) {
+		return fmt.Errorf("ccmap: Data already exists with given key %T", key)
+	}
+	
+	c.Set(key, val)
+
+	return nil
+}
+
+func (c *Cache[K, V]) Update(key K, val V) error {
+	if !c.Has(key) {
+		return fmt.Errorf("ccmap: Data doesn't exists with given key %T", key)
+	}
+
+	c.Set(key, val)
+
+	return nil
+}
+
+func (c *Cache[K, V]) Delete(key K) {
+	if c.Has(key) {
+		delete(c.Data, key)
+	}
+}
+
+func (c *Cache[K, V]) Flush() {
+	for k,v := range c.Data {
+		c.OnEvicted(k, v)
+		delete(c.Data, k)
+	}
+}
